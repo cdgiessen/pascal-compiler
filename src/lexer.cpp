6505ca72379177fc5259492c429d50ce746a3c52
Charles Giessen <cdgiessen@gmail.com>
2018-08-22 11:11:42 -0500

    fixed build issue, properly using fmt now

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,2 +1,18 @@
 #include <vector>
+#include <fmt/format.h>
 
+
+constexpr int line_buffer_length = 72;
+constexpr int identifier_length = 10;
+constexpr int integer_digit_length = 10;
+
+// xx.yyEzz
+constexpr int real_base_length = 5;
+constexpr int real_decimal_length = 5;
+constexpr int real_exponent_length = 2;
+
+int main(){
+
+    fmt::print("Hello, {}!", "world");
+    return 0;
+}
